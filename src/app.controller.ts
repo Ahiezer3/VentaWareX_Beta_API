7e2877c887d082d32ffd48df7d2f025ff69483f3
ahiezer3 <ahiezer3@gmail.com>
2025-12-15 02:08:25 -0600

    How to create a firt test user

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { UsersService } from './users/users.service';
+import { CreateUserDto } from './users/dto/create-user.dto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly usersService: UsersService) {}
 
   @Get()
   getHello(): string {
@@ -13,5 +15,25 @@ export class AppController {
   @Get('health')
   checkHealth(): string {
     return 'OK';
+  }
+
+  @Get('createTestUser')
+  createTestUser(): string {
+    var createUserDto:CreateUserDto  =  {
+      name: "Test",
+      lastName: "Test",
+      mothersLastName: "Test",
+      birthday: new Date(),
+      address: "Test",
+      zipCode: 12345,
+      country: "Test",
+      email: "test@test.com",
+      password: "Test123",
+      typeUser: 1,
+      status: 1
+    }
+
+    this.usersService.createUser(createUserDto);
+    return 'User "Test" created. \n Email: test@test.com \n Password: Test123';
   }
 }
